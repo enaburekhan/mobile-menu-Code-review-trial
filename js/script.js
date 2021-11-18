c0c7d5d01857ea972fcf5aafcda00aaf8789ae4d
enaburekhan <ericenaburekhan2018@gmail.com>
2021-11-18 08:54:03 +0100

    Fix linters errors

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-
-const menuIcon = document.querySelector('.hamburger-menu')
+const menuIcon = document.querySelector('.hamburger-menu');
 
 const navbar = document.querySelector('.navbar');
 
 menuIcon.addEventListener('click', () => {
-  navbar.classList.toggle('change')    
-})
+  navbar.classList.toggle('change');
+});
